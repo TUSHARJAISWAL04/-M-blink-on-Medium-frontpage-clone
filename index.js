6afcddfb1634ce2f786d4425e839d7d2028fb904
TUSHARJAISWAL04 <147417600+TUSHARJAISWAL04@users.noreply.github.com>
2023-10-09 20:36:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+setInterval(() => {
+    for (let i = 97; i < 113; i++) {
+        const rand1 = Math.floor(Math.random(28) * 27) + 1;
+        const rand2 = Math.floor(Math.random(28) * 27) + 1;
+        const rand3 = Math.floor(Math.random(28) * 27) + 1;
+        const rand4 = Math.floor(Math.random(28) * 27) + 1;
 
+        const currElement = document.getElementsByClassName(
+            String.fromCharCode(i) + rand1
+        );
+
+        const currElement2 = document.getElementsByClassName(
+            String.fromCharCode(i) + rand2
+        );
+
+        const currElement3 = document.getElementsByClassName(
+            String.fromCharCode(i) + rand3
+        );
+
+        const currElement4 = document.getElementsByClassName(
+            String.fromCharCode(i) + rand4
+        );
+
+        setTimeout(() => {
+            currElement[0].innerHTML = "";
+            currElement2[0].innerHTML = "";
+            currElement3[0].innerHTML = "";
+            currElement4[0].innerHTML = "";
+
+            setTimeout(() => {
+                currElement[0].innerHTML = "M";
+                currElement2[0].innerHTML = "M";
+                currElement3[0].innerHTML = "M";
+                currElement4[0].innerHTML = "M";
+            }, 800);
+        }, 800);
+    }
+}, 500);
